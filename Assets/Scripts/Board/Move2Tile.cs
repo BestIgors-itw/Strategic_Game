a2dae6841d0861cc5271403dc812d944307ebfd6
ip613s07 <vyacheslavgorohov@mail.ru>
2019-03-29 11:52:30 +0700

    add board

---
+++ Assets/Scripts/Board/Move2Tile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2Tile : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        player.transform.position = new Vector3(transform.position.x,
+            transform.position.y + transform.localScale.y / 2
+            + player.transform.localScale.y + 0.25f, transform.position.z);
+    }
+}
