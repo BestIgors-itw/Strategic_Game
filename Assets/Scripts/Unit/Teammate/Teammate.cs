91f49fc80952cfbb22163abfdcf0229ac4a9ff1a
ip613s07 <vyacheslavgorohov@mail.ru>
2019-04-15 23:02:05 +0700

    aimY mechanic

--- Assets/Scripts/Unit/Teammate/Teammate.cs
+++ Assets/Scripts/Unit/Teammate/Teammate.cs
@@ -15,6 +15,7 @@ public class Teammate : Unit
     {
         info = GetComponent<ObjectInfo>();
         agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Enemy");
     }
 
     // Update is called once per frame
@@ -36,5 +37,18 @@ public class Teammate : Unit
                 }
             }
         }
+
+        
+        float aimY = (Mathf.Atan2(transform.position.x - target.transform.position.x,
+            transform.position.z - target.transform.position.z) * 180f) / Mathf.PI;
+
+        aimY -= 90f;
+
+        if (aimY <= 180f)
+        {
+            aimY += 180f;
+        }
+
+        transform.rotation = Quaternion.Euler(0, aimY, 0);                
     }
 }
