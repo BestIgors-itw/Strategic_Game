8a14bc1f9b49f8b5fb87cb5e3b01d4fb887a046c
ip613s07 <vyacheslavgorohov@mail.ru>
2019-05-09 20:47:10 +0700

    add Attacking Enemies

--- Assets/Scripts/Unit/Teammate/Teammate.cs
+++ Assets/Scripts/Unit/Teammate/Teammate.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Teammate : Unit
-{    
+{
     private Ray ray;
     private RaycastHit hit;
 
@@ -12,12 +12,13 @@ public class Teammate : Unit
     {
         info = GetComponent<ObjectInfo>();
         agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = 0.3f;
 
-        gun = transform.Find("Gun").gameObject;        
+        gun = transform.Find("Gun").gameObject;
         shells = GameObject.Find("Shells").transform;
 
+        StartCoroutine(ChooseTarget("Enemy"));
         StartCoroutine(Fire());
-        StartCoroutine(ChooseTarget());
     }
 
     // Update is called once per frame
@@ -33,7 +34,7 @@ public class Teammate : Unit
                 {
                     info.isSelected = false;
 
-                    destination = hit.collider.gameObject;
+                    destination = hit.collider.transform;
 
                     MoveTo(destination);
                 }
@@ -42,110 +43,4 @@ public class Teammate : Unit
 
         Aim();
     }
-
-    private IEnumerator ChooseTarget()
-    {
-        while (true)
-        {            
-            targets = GameObject.FindGameObjectsWithTag("Enemy");
-
-            if (targets != null)
-            {
-                GameObject closest = null;
-                float closest_sqrDistance = 0f;
-                float check_sqrDistance;
-
-                foreach (GameObject obj in targets)
-                {
-                    if (closest_sqrDistance == 0f)
-                    {
-                        closest = obj;
-                        closest_sqrDistance = (obj.transform.position
-                            - transform.position).sqrMagnitude;
-                    }
-                    else
-                    {
-                        check_sqrDistance = (obj.transform.position
-                            - transform.position).sqrMagnitude;
-
-                        if (closest_sqrDistance > check_sqrDistance)
-                        {
-                            closest = obj;
-                            closest_sqrDistance = check_sqrDistance;
-                        }
-                    }
-                }
-
-                target = closest;
-            }
-            else
-            {
-                target = null;
-            }
-
-            yield return new WaitForSeconds(0.3f);
-        }
-    }
-
-    protected void Aim()
-    {
-        float distance;
-
-        Vector3 heading;
-        Vector3 direction;
-
-        if (target == null)
-        {
-            isAimed = false;
-        }
-        else
-        {
-            heading = target.transform.position - transform.position;
-
-            distance = heading.magnitude;
-
-            direction = heading / distance;
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,
-                Quaternion.LookRotation(new
-                Vector3(direction.x, 0, direction.z)), Time.deltaTime * 120f);
-
-            if (Vector3.Angle(new Vector3(heading.x, 0, heading.z), new Vector3(transform.forward.x, 0, transform.forward.z)) < 10.0f)
-            {
-                gun.transform.rotation = Quaternion.RotateTowards(gun.transform.rotation,
-                Quaternion.LookRotation(direction)
-                * Quaternion.Euler(90, 0, 0), Time.deltaTime * 40f);
-
-                if (Vector3.Angle(heading, gun.transform.up) < 10.0f)
-                {
-                    if (distance > target_range)
-                    {
-                        isAimed = false;
-                    }
-                    else
-                    {
-                        isAimed = true;
-                    }
-                }
-            }
-            else
-            {
-                isAimed = false;
-            }
-        }
-    }
-
-    private IEnumerator Fire()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(1.5f);
-
-            if (isAimed) {
-                GameObject sh = Instantiate(shell, gun.transform.position, gun.transform.rotation, shells.transform);
-                Physics.IgnoreCollision(sh.GetComponent<Collider>(), GetComponent<Collider>());
-                sh.transform.up = gun.transform.up;
-            }
-        }
-    }    
 }
