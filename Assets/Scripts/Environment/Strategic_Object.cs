e06d72a13d413f46cf03e2f604afa4a1d1886818
ip613s07 <vyacheslavgorohov@mail.ru>
2019-05-11 10:47:13 +0700

    add environment destruction

---
+++ Assets/Scripts/Environment/Strategic_Object.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strategic_Object : MonoBehaviour
+{
+    public float health;
+
+    protected void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Shell")
+        {
+            health -= collision.gameObject.GetComponent<Shell>().damage;
+
+            if (health <= 0)
+            {
+                Destroy(gameObject, 0.5f);
+            }
+        }
+    }
+}
